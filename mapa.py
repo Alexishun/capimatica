e88a468e4afec7c6cbb533b0bd488cc88ffeb5cf
Alexis Diaz <alexis.diaz.h@uni.pe>
2025-10-04 16:15:58 -0500

    mapaaa

--- mapa.py
+++ mapa.py
@@ -2,7 +2,8 @@ from datetime import datetime
 import streamlit as st
 import altair as alt
 import vega_datasets
-
+import folium
+from streamlit_folium import st_folium
 
 full_df = vega_datasets.data("seattle_weather")
 
@@ -30,7 +31,22 @@ dataset](https://altair-viz.github.io/case_studies/exploring-weather.html)!
 """
 
 ""
+st.title("Selecciona un lugar en el mapa 🌍")
+# Crear mapa centrado en Perú
+m = folium.Map(location=[-9.19, -75.0152], zoom_start=5)
 
+# Habilitar clics en el mapa
+m.add_child(folium.LatLngPopup())
+
+# Mostrar mapa en Streamlit
+st_data = st_folium(m, width=700, height=500)
+
+# Si el usuario hizo clic, mostramos las coordenadas
+if st_data["last_clicked"] is not None:
+    lat = st_data["last_clicked"]["lat"]
+    lon = st_data["last_clicked"]["lng"]
+    st.success(f"Coordenadas seleccionadas: Latitud = {lat:.6f}, Longitud = {lon:.6f}")
+    
 df_2015 = full_df[full_df["date"].dt.year == 2015]
 df_2014 = full_df[full_df["date"].dt.year == 2014]
 
